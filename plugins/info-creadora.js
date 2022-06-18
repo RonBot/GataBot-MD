a0c1e96403708d760cf2652efd4561b0b0967ff1
Roncito <96086954+RonBot@users.noreply.github.com>
2022-06-17 21:26:54 -0500

    Add files via upload

--- plugins/info-creadora.js
+++ plugins/info-creadora.js
@@ -2,26 +2,24 @@ import fs from 'fs'
 function handler(m, { conn }) {
 let text = `
 *𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙤 | 𝘾𝙤𝙣𝙩𝙖𝙘𝙩* 
-*Wa.me/593993684821 (BOT)*
-*Wa.me/51993042301 (BOT)*
-*https://www.instagram.com/gata_dios*
+*Wa.me/573023900508 (NO BOT)*
+*Wa.me/37122262180 (BOT)*
 `.trim()   
 conn.reply(m.chat, text, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
-body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫𝐚 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
+title: '𝑲𝑨𝑵 - 𝑩𝑶𝑻🌙 | by kanixxx',
+body: '𝐂𝐫𝐞𝐚𝐝𝐨𝐫 | 𝐂𝐫𝐞𝐚𝐭𝐨𝐫',         
 previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
-sourceUrl: `https://wa.me/593993684821`}}})
+sourceUrl: `https://wa.me/573023900508`}}})
   
 //const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 //this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
   
 let pp = './media/menus/Menu2.jpg'
-let str = `🧡 *Eso son los contactos para ti.*\n💜 *That's the contacts for you.*`
+let str = `🥀 *Eso son los contactos para ti.*\n🥀 *That's the contacts for you.*`
 
 conn.sendHydrated(m.chat, str, wm, pp, 'https://www.instagram.com/gata_dios', '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['🐈 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
-['🎁 𝘿𝙤𝙣𝙖𝙧 | 𝘿𝙤𝙣𝙖𝙩𝙚', '.donar'],
+['🌙 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣 | 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙩𝙞𝙤𝙣', '.infobot'],
 ['☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩', '/menu']
 ], m,)
 }
